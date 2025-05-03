2c3f3414a612a5a7ca84613bced9aa459fce9bf1
AlvarezJoseR <75347678+AlvarezJoseR@users.noreply.github.com>
2025-05-03 10:03:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true,
-    allowedHosts: ['all']
+    allowedHosts: ['proyectos.fireploy.online','all']
   }
 });
