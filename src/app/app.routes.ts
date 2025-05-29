3f5fc3cb88e2d6e4298a2da1e1971911176b184c
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2025-05-28 23:02:58 -0500

    fix?

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeTemplateComponent } from './home-template/home-template.component';
 import { InitialContentComponent } from './pages/initial-content/initial-content.component';
 import { ContentComponent } from './pages/content/content.component';
+import { BrowserModule  } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
   {
@@ -16,7 +19,9 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule],
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+    bootstrap: [AppComponent],
 })
-export class AppRoutingModule {}
+export class AppModule {}
