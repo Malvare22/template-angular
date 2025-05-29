a9d6ed0006d9e23762e09e152030f53168b0500a
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2025-05-28 22:58:28 -0500

    FIX?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,9 @@ import { DOCUMENT } from '@angular/common'; // Usamos @angular/common
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [{ provide: APP_BASE_HREF, useValue: environment.basePath }], // Usa el valor desde environment.basePath
 })
 export class AppComponent {
   constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  ngOnInit(): void {
-    // Accedemos a todos los elementos <base> en el documento
-    const bases = this.document.getElementsByTagName('base');
-
-    // Verificamos que haya al menos un elemento <base>
-    if (bases.length > 0) {
-      // Establecemos dinámicamente el atributo href de la primera etiqueta <base>
-      bases[0].setAttribute('href', environment.basePath);
-    }
-  }
 
   title = 'template-Angular';
 }
